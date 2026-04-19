c1c0c4b71ef0562e6ecc3e034d1d6a8dee1fb524
skwaterpuzzle <skmohanto80@gmail.com>
2026-04-19 09:35:55 +0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: "https://stuafzrbmxspvkvnhzwh.supabase.co",
-  anonkey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0dWFmenJibXhzcHZrdm5oendoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MzYxMjYsImV4cCI6MjA5MjAxMjEyNn0.7jZ4Zhody2Ca-hR2wiq3t8S1IolzY9S-lwGKlMUluaQ"
+  anonkey: "sb_publishable_mV6hyZ9BoMnIxiF787fNxg_ytMQcqtv"
 };
